d768e70f03ed4ea0c67fcd88d2b8f0bc8c1f56cd
Ronan Souza <ronan.souza@wildlifestudios.com>
2020-12-04 08:26:37 -0300

    Add more results to mock

--- backend/server.js
+++ backend/server.js
@@ -21,23 +21,30 @@ const seed = () => {
   return Promise.all([
     db.pacients.create({name: "Ronan", id: 111, age: 25, address: "Rua Wilson Pereira", gender: "male"}),
     db.pacients.create({name: "Fernanda", id: 112, age: 24, address: "Avenida Almirante Barroso", gender: "female"}),
-    db.exams.create({name: "Glicose", code: "GLIC", ref_value: {male_upper: 120, male_lower: 60, female_upper: 120, female_lower: 60}}),
-    db.exams.create({name: "Colesterol", code: "COL", ref_value: {male_upper: 120, male_lower: 60, female_upper: 120, female_lower: 60}}),
+    db.exams.create({name: "Glicose", description: "Glicose ou glicemia em jejum serve para medir o nível da glicose na circulação sanguínea do paciente", code: "GLIC", ref_value: {male_upper: 120, male_lower: 60, female_upper: 120, female_lower: 60}}),
+    db.exams.create({name: "Colesterol", description: "", code: "COL", ref_value: {male_upper: 120, male_lower: 60, female_upper: 120, female_lower: 60}}),
     db.exams.create({name: "Triglicerídeos", code: "TRIG", ref_value: {male_upper: 120, male_lower: 60, female_upper: 120, female_lower: 60}}),
+    db.exams.create({name: "Creatinina", description: "Creatinina é usado para avaliar a função dos rins", code: "CRE", ref_value: {male_upper: 1.0, male_lower: 0.5, female_upper: 0.5, female_lower: 1.0}}),
     db.results.create({value: 80}),
     db.results.create({value: 90}),
+    db.results.create({value: 150}),
+    db.results.create({value: 1}),
     db.results.create({value: 150}),
     db.treatments.create({}),
     db.treatments.create({})
   ])
-  .then(([ronan, fernanda, glic, col, trig, result1, result2, result3, t1, t2]) => {
+  .then(([ronan, fernanda, glic, col, trig, cre, result1, result2, result3, result4, result5, t1, t2]) => {
     return Promise.all([
       result1.setExam(glic),
       result2.setExam(col),
       result3.setExam(trig),
+      result4.setExam(cre),
+      result5.setExam(glic),
       result1.setTreatment(t1),
       result2.setTreatment(t1),
       result3.setTreatment(t2),
+      result4.setTreatment(t2),
+      result5.setTreatment(t2),
       t1.setPacient(fernanda),
       t2.setPacient(ronan)
     ]);
